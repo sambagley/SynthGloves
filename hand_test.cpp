14ab9f20f37a3fcaed54e229d22373013f001bf1
sambagley <sambagley@byui.edu>
2019-06-27 13:36:09 -0600

    displays on one line now

--- hand_test.cpp
+++ hand_test.cpp
@@ -5,19 +5,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <chrono>
+//#include <string.h>
 using namespace std;
 
 int main(){
 	
+	
+	
+	
 	Hand handy("/dev/ttyACM0",9600);
 	
-	
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int d = 0;
+		int e = 0;
+		int f = 0;
+		int g = 0;
+		int h = 0;
 	while(1)
 	{
 		handy.updateHand();
-		cout << handy.getMiddle() << endl;
-		cout << handy.getIndex() << endl;
-		//sleep_for(milliseconds(10));
+		a = handy.getThumb();
+		b = handy.getIndex();
+		c = handy.getMiddle();
+		d = handy.getRing();
+		e = handy.getPinky();
+		f = handy.getXAng();
+		g = handy.getYAng();
+		h = handy.getZAng();
+		cout << "\r " <<  a << " ";
+		cout << b << " ";
+		cout << c << " ";
+		cout << d << " ";
+		cout << e << " ";
+		cout << f << " ";
+		cout << g << " ";
+		cout << h << " ";
+		cout.flush();
+		usleep(100000);
+		
 	}
 	
 	
