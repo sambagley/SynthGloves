5c67e063b1214416cba65f8feefb1ffd712abbad
nathjenson <nathanljenson@gmail.com>
2019-07-17 22:23:45 -0600

    Added drum kits to looper samples

---
+++ build_debug.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+g++ -g -std=c++11 main_synth.cpp sine.cpp instrument.cpp scaleFunctions.cpp hand.cpp instrument_finger_keys.cpp instrument_looper.cpp instrument_chords.cpp /home/pi/SynthGloves/AudioFile/AudioFile.cpp -lwiringPi -lpthread -ljack -ljackcpp synth_test 
+
